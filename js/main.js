46549def7466214e40f6bb2aefb783c92c3b5cd6
JocelynJoe <jocelyn.pena.contreras@gmail.com>
2016-11-08 09:33:54 -0800

    agregar lista agregar tarea

--- js/main.js
+++ js/main.js
@@ -7,27 +7,50 @@ function agregarLista() {
 	//crear div para contener nuevo menu
 	var contieneNuevo=document.createElement('div');
 	contieneNuevo.classList.add('caja-blanca');
+	
 
 	//crear textarea y sus atributos
 	var inputTexto = document.createElement('textarea');
 	inputTexto.cols =30;
 	inputTexto.rows =1;
 	inputTexto.setAttribute("placeholder","Añadir una lista");
-	inputTexto.classList.add("ingresar");
+	inputTexto.classList.add("ingresar","form-control");
+	inputTexto.setAttribute('id','ingresar-lista');
 	//crear boton guardar
 	var botonAgregar=document.createElement('button');
-	botonAgregar.classList.add("btn","btn-primary","bto-guardar");
+	botonAgregar.classList.add("btn","btn-success","bto-guardar");
 	var texto=document.createTextNode('Guardar');
+	
 	botonAgregar.appendChild(texto);
-
+	//asignar hijos a contenedor
 	contieneNuevo.appendChild(inputTexto);
 	contieneNuevo.appendChild(botonAgregar);
 	contiene.appendChild(contieneNuevo);
-	//asignar hijos a contenedor
-	//contiene.appendChild(inputTexto);
-	//contiene.appendChild(botonAgregar);
 
-	
+	botonAgregar.addEventListener('click', function(){
+		contiene.classList.toggle('mover');
+		var nuevaLista=document.getElementById('ingresar-lista').value;
+ 		var nuevoSpan=document.createTextNode(nuevaLista);
+ 		var nuevaCol=document.getElementById('cont-tarea-new');
+ 		var divNuevo=document.createElement('div');
+ 		var nuevaTarea=document.createElement('a');
+ 		var textoNueva=document.createTextNode("Añadir nueva tarea");
+ 		nuevaTarea.classList.add("displayB");
+ 		divNuevo.classList.add('caja-blanca-tarea','text-center');
+ 		nuevaTarea.appendChild(textoNueva);
+ 		divNuevo.appendChild(nuevoSpan);
+ 		divNuevo.appendChild(nuevaTarea);
+ 		nuevaCol.appendChild(divNuevo);
+ 		var borrar=document.getElementById('ingresar-lista');
+		var papaBorar=borrar.parentNode;
+		papaBorar.parentNode.removeChild(papaBorar);
+ 		agregarLista();
+ 		divAna.classList.toggle('anadir-lista-borrar');
+ 		
+	})
+ 		
+ 	
 }
 
- 
+
+
